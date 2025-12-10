81d0223e5a73861b96d5a0b9a99a7fd6ad6edbfc
ExoCode33 <emmanuel@supracode.ca>
2025-12-10 16:47:23 -0500

    Update characterProfile.js

--- src/components/embeds/characterProfile.js
+++ src/components/embeds/characterProfile.js
@@ -72,11 +72,11 @@ export async function buildCharacterProfileEmbed(user, characters, interaction =
   mainSection += `\u001b[1;34m🎮 IGN:\u001b[0m ${mainChar.ign}\n`;
   mainSection += `\u001b[1;34m🆔 UID:\u001b[0m ${mainChar.uid}\n`;
   mainSection += `\n`;
-  mainSection += `\u001b[1;34m🏰 Guild:\u001b[0m ${mainChar.guild || 'None'}\n`;
   mainSection += `\u001b[1;34m🎭 Class:\u001b[0m ${mainChar.class}\n`;
   mainSection += `\u001b[1;34m📋 Subclass:\u001b[0m ${mainChar.subclass} ${roleEmoji}\n`;
-  mainSection += `\n`;
   mainSection += `\u001b[1;34m💪 Score:\u001b[0m ${formatAbilityScore(mainChar.ability_score)}\n`;
+  mainSection += `\n`;
+  mainSection += `\u001b[1;34m🏰 Guild:\u001b[0m ${mainChar.guild || 'None'}\n`;
   mainSection += `\u001b[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n`;
   mainSection += '```';
 
@@ -88,7 +88,8 @@ export async function buildCharacterProfileEmbed(user, characters, interaction =
       const subRoleEmoji = sub.role === 'Tank' ? '🛡️' : sub.role === 'DPS' ? '⚔️' : '💚';
       if (i > 0) subSection += `\u001b[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n`;
       else subSection += `\u001b[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n`;
-      subSection += `\u001b[1;34m🎭 Class:\u001b[0m ${sub.class} - ${sub.subclass} ${subRoleEmoji}\n`;
+      subSection += `\u001b[1;34m🎭 Class:\u001b[0m ${sub.class}\n`;
+      subSection += `\u001b[1;34m📋 Subclass:\u001b[0m ${sub.subclass} ${subRoleEmoji}\n`;
       subSection += `\n`;
       subSection += `\u001b[1;34m💪 Score:\u001b[0m ${formatAbilityScore(sub.ability_score)}\n`;
     });
@@ -104,10 +105,12 @@ export async function buildCharacterProfileEmbed(user, characters, interaction =
       if (i > 0) altSection += `\u001b[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n`;
       else altSection += `\u001b[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n`;
       altSection += `\u001b[1;34m🎮 IGN:\u001b[0m ${alt.ign}   \u001b[1;34m🆔 UID:\u001b[0m ${alt.uid}\n`;
-      altSection += `\u001b[1;34m🏰 Guild:\u001b[0m ${alt.guild || 'None'}\n`;
-      altSection += `\u001b[1;34m🎭 Class:\u001b[0m ${alt.class} - ${alt.subclass} ${altRoleEmoji}\n`;
       altSection += `\n`;
+      altSection += `\u001b[1;34m🎭 Class:\u001b[0m ${alt.class}\n`;
+      altSection += `\u001b[1;34m📋 Subclass:\u001b[0m ${alt.subclass} ${altRoleEmoji}\n`;
       altSection += `\u001b[1;34m💪 Score:\u001b[0m ${formatAbilityScore(alt.ability_score)}\n`;
+      altSection += `\n`;
+      altSection += `\u001b[1;34m🏰 Guild:\u001b[0m ${alt.guild || 'None'}\n`;
     });
     altSection += `\u001b[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n`;
     altSection += '```';
