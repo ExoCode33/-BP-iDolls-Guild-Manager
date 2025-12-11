6a42fe1618fd478c9c9393a98c7e3a5f7db5cb0b
ExoCode33 <emmanuel@supracode.ca>
2025-12-11 16:41:35 -0500

    Update view-character.js

--- src/commands/user/view-character.js
+++ src/commands/user/view-character.js
@@ -1,5 +1,5 @@
-import pkg from 'discord.js';
-const { SlashCommandBuilder } = pkg;
+import discord from 'discord.js';
+const { SlashCommandBuilder } = discord;
 import { buildCharacterProfileEmbed } from '../../components/embeds/characterProfile.js';
 import db from '../../services/database.js';
 import logger from '../../utils/logger.js';
