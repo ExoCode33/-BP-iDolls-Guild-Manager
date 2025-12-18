59c0d6baf05adf100c22fe82d819ef532bee0bdd
ExoCode33 <emmanuel@supracode.ca>
2025-12-18 11:55:59 -0500

    Update embeds.js

--- src/ui/embeds.js
+++ src/ui/embeds.js
@@ -46,10 +46,12 @@ export async function profileEmbed(user, characters, interaction = null) {
   const timeText = tz ? `\n🌍 ${formatTime(tz)}` : '';
 
   if (!main) {
+    const joinHealEmoji = interaction?.guild?.emojis.cache.find(e => e.name === 'HelloThere');
+    const emojiStr = joinHealEmoji ? `${joinHealEmoji} ` : '';
+    
     return new EmbedBuilder()
       .setColor(COLORS.PRIMARY)
-      .setAuthor({ name: 'Join Heal', iconURL: 'https://cdn.discordapp.com/emojis/1451244468881591427.png' })
-      .setDescription(`\`\`\`ansi\n\u001b[35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n\n    \u001b[1;34m♡₊˚\u001b[0m \u001b[1;34mWelcome\u001b[0m \u001b[33m${displayName}\u001b[0m \u001b[1;34m˚₊♡\u001b[0m\n\n    \u001b[1;34mNo character yet? No worries~\u001b[0m\n\n    \u001b[1;34m✿ Tap the button below\u001b[0m\n    \u001b[1;34m✿ We'll set you up in no time!\u001b[0m\n\n\u001b[35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n\`\`\``)
+      .setDescription(`## ${emojiStr}**Join Heal**\n\`\`\`ansi\n\u001b[35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n\n    \u001b[1;34m♡₊˚\u001b[0m \u001b[1;34mWelcome\u001b[0m \u001b[33m${displayName}\u001b[0m \u001b[1;34m˚₊♡\u001b[0m\n\n    \u001b[1;34mNo character yet? No worries~\u001b[0m\n\n    \u001b[1;34m✿ Tap the button below\u001b[0m\n    \u001b[1;34m✿ We'll set you up in no time!\u001b[0m\n\n\u001b[35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m\n\`\`\``)
       .setTimestamp();
   }
 
